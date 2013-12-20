a7d320548bce0074c8d9de832b9895757aa3b934
Yi Gu <ghawk.gu@gmail.com>
2013-12-20 13:29:12 +0900

    Creating the bak and tmp dirs is no longer needed.

--- init-setup.sh
+++ init-setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 VIMFILES=$HOME/.vim
-mkdir $VIMFILES/tmp
-mkdir $VIMFILES/bak
+# mkdir $VIMFILES/tmp
+# mkdir $VIMFILES/bak
 
 git clone https://github.com/gmarik/vundle.git $VIMFILES/bundle/vundle
 
